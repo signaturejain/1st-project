79367d411db2f27913cec926b31a9a1b8f46d643
signaturejain <57012600+signaturejain@users.noreply.github.com>
2019-10-25 23:05:17 +0530

    make it random

--- straight pyramid.c
+++ straight pyramid.c
@@ -2,9 +2,11 @@
 
 int main()
 {
-    int i,j;
+    int i,j,n;
+    printf("no of lines");
+ scanf("%d",n);
     
-    for(i=1;i<=4;i++)
+    for(i=1;i<=n;i++)
     {
     for(j=1;j<=i;j++)
     {printf("*");}
